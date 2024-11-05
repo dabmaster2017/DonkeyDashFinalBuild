a8b4c8170c64ea7d090f6a5ad9812f0ecc78a4a3
taeva <161221109+dabmaster2017@users.noreply.github.com>
2024-11-05 19:43:06 +1100

    Add files via upload

---
+++ GameManagerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public GameObject CharacterUI;
+
+    public void gameOver()
+    {
+        CharacterUI.SetActive(false);
+        gameOverUI.SetActive(true);
+
+    }
+
+    public void restart()
+    {
+        Debug.Log("Restart button clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
